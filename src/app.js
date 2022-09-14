d4f013d4c0168893122ce695548bc8d7995dde9c
Dustin Krysak <dustin@bashfulrobot.com>
2022-09-14 16:47:33 -0700

    chore: :art: changed message to trigger scan
    Changes to be committed:
    	modified:   src/app.js

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const server = http.createServer((req, res) => {
   fs.readFile("./mydata.json", "utf8", (err, jsonString) => {
     if (err) {
       console.log("File read failed:", err);
-      const printme = "Hello Sysdig Workshop!"
+      const printme = "Hello Sysdig Workshop - With security scan!"
       res.end(printme)
     }
     const printme = jsonString
